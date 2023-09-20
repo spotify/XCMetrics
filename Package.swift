d8212ff13281e5cda2730b7e8c03c638e6ad70ab
Rafa Marcén <rafa_marcen@hotmail.com>
2023-09-20 11:46:02 +0200

    Increase minimum version to macOS 13.0 (#101)
    * Increase minimum version to macOS 13.0
    
    * Increase Xcode version to 14.3.1 and Swift version to 5.7
    
    * Switch back to Xcode 14.2
    
    * Switch back to Xcode 14.2
    
    * Bump to macOS 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "XCMetrics",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v13),
     ], products: [
         .executable(name: "XCMetrics", targets: ["XCMetricsApp"]),
         .executable(name: "XCMetricsBackend", targets: ["XCMetricsBackend"]),
